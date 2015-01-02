fe1267f69dfe1422ab0fea9036dd1f5570d6baed
LivingInSyn <jeremymill@gmail.com>
2015-01-02 10:43:01 -0500

    Working
    This is basically working right now.

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
 from kivy.properties import ListProperty, StringProperty, \
         NumericProperty, BooleanProperty
 from kivy.uix.boxlayout import BoxLayout
-#from kivy.uix.floatlayout import FloatLayout
 from kivy.clock import Clock
 from kivy.uix.label import Label
 from kivy.core.window import Window
@@ -25,22 +24,32 @@ class Logout_Time(Screen):
 
 
 class Logout_App(App):
+
+	'''set the window title and the icon here''' ''' ''' 
+	title = 'HTC Logoff Utility'
+	#icon = 'htc_logoff_icon.png'
+
 	def build(self):
 		self.logout_times = Logout_Time(name='logout')
 		self.transition = SlideTransition(duration=.35)
 		root = ScreenManager(transition=self.transition)
 		root.add_widget(self.logout_times)
+		'''make the close window binding and initialize self.time to 0'''
 		Window.bind(on_close=self.window_closed)
+		self.time = 0
 		return root
 		
 	def set_time(self,time):
 		self.time = time
 		if self.time==45:
 			self.time = self.time*60
+			#next line is for debugging
+			#self.time = 0
 			Window.close()
 		elif self.time==1:
 			self.time = self.time*3600
-			self.time = 7
+			#next line is for debugging
+			#self.time = 7
 			Window.close()
 		elif self.time==2:
 			self.time = self.time*3600
@@ -49,16 +58,17 @@ class Logout_App(App):
 			self.time = self.time*3600
 			Window.close()
 			
-		
-	'''def on_stop(self):
-		print("I CLOSED")
-		subprocess.Popen(['notepad.exe','test.txt'])
-	'''
 	
 	def window_closed(self, event):
 		print("window closed")
-		time.sleep(self.time)
-		subprocess.Popen(["shutdown.exe","/l"])
+		if self.time==0:
+			#pass
+			'''This Next line will have to change to the exe once it's made. It's to relaunch the app if someone closes
+			it, forcing them to enter a time'''
+			subprocess.Popen(["C:\uits\Kivy-1.8.0-py2.7-win32\kivy.bat","C:\Users\student1953\Documents\GitHub\Py_LogOff\main.py"])
+		else:
+			time.sleep(self.time)
+			subprocess.Popen(["shutdown.exe","/l"])
 		exit()
 
 if __name__ == '__main__':
