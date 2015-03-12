96251dac8ececf03cdb2d4fd54d5418f8fc3f8af
LivingInSyn <jeremymill@gmail.com>
2015-03-12 07:58:13 -0400

    registry keys

--- installer/installer.py
+++ installer/installer.py
@@ -35,11 +35,21 @@ def install():
         util.copy(item,directory)
     #make the runtime registry key
     #note the KEY ALL ACCESS is important, and so are the escaped quotes in the key
+    #this one is tested and working
     start_run_key = wreg.OpenKey(wreg.HKEY_LOCAL_MACHINE, "Software\\Microsoft\\Windows\\CurrentVersion\\Run",0,wreg.KEY_ALL_ACCESS)
     wreg.SetValueEx(start_run_key, 'LogOff Utility', 0, wreg.REG_SZ, "\""+directory+"HTC-LogOut.exe"+"\"")
     wreg.CloseKey(start_run_key)
     
-    #now start the uninstall string
+    #create the subkey
+    uninstall_subkey = wreg.OpenKey(wreg.HKEY_LOCAL_MACHINE, "Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall",0,wreg.KEY_ALL_ACCESS)
+    wreg.SetValue(uninstall_subkey, 'Logoff_Utility', wreg.REG_SZ, '')
+    wreg.CloseKey(uninstall_subkey)
+    #create the uninstall information
+    uninstall_key = wreg.OpenKey(wreg.HKEY_LOCAL_MACHINE, "Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Logoff_Utility",0,wreg.KEY_ALL_ACCESS)
+    wreg.SetValueEx(uninstall_key, 'DisplayName', 0, wreg.REG_SZ, "HTC Logoff Utility")
+    #as a side note...this uninstaller doesn't exist...yet....
+    wreg.SetValueEx(uninstall_key, 'UninstallString', 0, wreg.REG_SZ, "\""+directory+"uninstall.exe"+"\"")
+    wreg.CloseKey(uninstall_key)
     
         
         
